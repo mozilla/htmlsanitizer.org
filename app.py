16fdd42d6bba474d33a3a404636c7b57c26f5057
Brian J Brennan <brianloveswords@gmail.com>
2012-09-15 18:25:53 -0400

    MVP done

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
 import os
 import bleach
+import json
 from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 app.debug = True
 
+VALID_OPTIONS = [
+    'text',
+    'tags',
+    'attributes',
+    'styles',
+    'strip',
+    'strip_comments'
+]
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return show_index()
-    return bleach_content()
+    return handle_post()
 
 def show_index(): 
    return render_template('index.html')
 
-def bleach_content():
-    return pull_content(request);
+def handle_post():
+    return parse_request(request);
 
-def pull_content(request):
+def parse_request(request):
     content_type = request.headers.get('Content-Type')
     methods = {
         'application/json': handle_json,
@@ -29,10 +39,31 @@ def pull_content(request):
     return handler(request)
 
 def handle_json(request):
-    return 'lol, json'
+    body = request.data
+    try:
+        options = json.loads(body)
+    except ValueError:
+        return "Could not parse request. Make sure your JSON is valid", 400
+    return sanitize(options, 'json')
 
 def handle_form(request):
-    return 'lol, form'
+    if not request.form:
+        return "Could not read form data.", 400
+    return sanitize(request.form, 'form')
+
+def sanitize(options, content_type):
+    if not options.get('text'):
+        return "Didn't find any text to bleach. Make sure you pass a value for `text` in your JSON object", 400
+    options = remove_invalid_options(options)
+    return bleach.clean(**options)
+
+def remove_invalid_options(options):
+    valid_options = {}
+    for opt in VALID_OPTIONS: 
+        value = options.get(opt)
+        if value:
+            valid_options[opt] = value
+    return valid_options
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
